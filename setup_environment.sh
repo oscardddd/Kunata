de2e9c0cbb4417c8a8a0b81e7b2ff6774d16084a
Oscardd5 <Oscardd5@node3.xx.gaea-pg0.wisc.cloudlab.us>
2024-12-19 21:32:37 -0600

    update setup env

--- setup_environment.sh
+++ setup_environment.sh
@@ -14,5 +14,5 @@ conda activate test
 
 echo "Installing Python packages"
 pip3 install --upgrade pip
-conda install pytorch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 cpuonly -c pytorch
+conda install pytorch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 cpuonly -c pytorch -y
 pip3 install numpy
